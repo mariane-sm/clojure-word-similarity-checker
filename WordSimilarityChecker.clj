28d9cc0832013c3518f23f8eac518e68826156c5
Mariane Siqueira Machado <mariane.machado@gmail.com>
2014-02-21 22:56:02 -0300

    defining maxNumberOfDiffentLetters

--- WordSimilarityChecker.clj
+++ WordSimilarityChecker.clj
@@ -1,11 +1,13 @@
 
+(def maxNumberOfDifferentLetters 1)
+
 (defn haveAllowedQuantityOfExtraLetters [word1 word2]
   (if (.contains word1 word2)
-    (isQuantityOfDifferentLettersAcceptable word1 word2 1)
+    (isQuantityOfDifferentLettersAcceptable word1 word2)
     (if (.contains word2 word1)
-      (isQuantityOfDifferentLettersAcceptable word2 word1 1) false)))
+      (isQuantityOfDifferentLettersAcceptable word2 word1) false)))
 
-(defn isQuantityOfDifferentLettersAcceptable [word1 word2 maxNumberOfDifferentLetters]
+(defn isQuantityOfDifferentLettersAcceptable [word1 word2]
   (<= (- (count word1) (count word2)) maxNumberOfDifferentLetters))
 
 (defn compareSameSizeWordEqualityLetterByLetter [word1 word2]
@@ -16,7 +18,7 @@
   (<= (- 
         (count (compareSameSizeWordEqualityLetterByLetter word1 word2))
         (count (filter true? (compareSameSizeWordEqualityLetterByLetter word1 word2)))) 
-      1))
+      maxNumberOfDifferentLetters))
   
 (defn isSimilar [word1 word2]
   (if (= 
